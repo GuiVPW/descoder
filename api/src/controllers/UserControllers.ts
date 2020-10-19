a8b8567c9be0c1110e4876075580d2d5ef4ae55f
Xubili <guivpw68@gmail.com>
2020-10-19 14:28:42 -0300

    added try catch to generate errors

--- api/src/controllers/UserControllers.ts
+++ api/src/controllers/UserControllers.ts
@@ -6,36 +6,52 @@ export const indexAll = async (
 	req: Request,
 	res: Response
 ): Promise<Response> => {
-	const mentores = await User.find().sort('-createdAt')
+	try {
+		const mentores = await User.find().sort('-createdAt')
 
-	if (!mentores) return res.status(502).json('Erro na solicitação...')
+		if (!mentores) return res.status(502).json('Erro na solicitação...')
 
-	return res.json(mentores)
+		return res.json(mentores)
+	} catch (e) {
+		console.log(e)
+		return res.status(502).json('Não foi possível criar seu usuário...')
+	}
 }
 
 export const index = async (req: Request, res: Response): Promise<Response> => {
-	const { id } = req.query
-	const mentor = await User.findById(id).sort('-createdAt')
+	try {
+		const { id } = req.query
+		const mentor = await User.findById(id).sort('-createdAt')
 
-	if (!mentor) return res.status(502).json('Usuário não encontrado...')
+		if (!mentor) return res.status(502).json('Usuário não encontrado...')
 
-	return res.json(mentor)
+		return res.json(mentor)
+	} catch (e) {
+		console.log(e)
+		return res.status(502).json('Não foi possível criar seu usuário...')
+	}
 }
 
 export const store = async (req: Request, res: Response): Promise<Response> => {
-	const { nome, email, descrição, calendly, skills, empresa } = req.body
+	try {
+		console.log(req.body)
+		const { nome, email, descrição, calendly, skills, empresa } = req.body
 
-	const newUser = await User.create({
-		nome,
-		email,
-		descrição,
-		calendly,
-		skills,
-		empresa
-	})
+		const newUser = await User.create({
+			nome,
+			email,
+			descrição,
+			calendly,
+			skills: skills.split(',').map((skill: string) => skill.trim()),
+			empresa
+		})
 
-	if (!newUser)
+		if (!newUser)
+			return res.status(502).json('Não foi possível criar seu usuário...')
+
+		return res.json(newUser)
+	} catch (e) {
+		console.log(e)
 		return res.status(502).json('Não foi possível criar seu usuário...')
-
-	return res.json(newUser)
+	}
 }
