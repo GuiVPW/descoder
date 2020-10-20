abc11435b32ad8acd1ebdf2a2dfcc5a97584bdc8
Xubili <guivpw68@gmail.com>
2020-10-20 04:50:40 -0300

    removed BodyParser and added express middleware

--- api/index.ts
+++ api/index.ts
@@ -1,18 +1,12 @@
 import express from 'express'
 import cors from 'cors'
 import routes from './src/constants/routes'
-import BodyParser from 'body-parser'
 
 const app = express()
 
-app.use(BodyParser.json())
-app.use(
-	BodyParser.urlencoded({
-		extended: false
-	})
-)
-
 app.use(cors())
+
+app.use(express.json())
 
 app.use(routes)
 
