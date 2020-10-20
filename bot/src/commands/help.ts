03904b61dc3c579f5a57df17e43c355ff6ddbd24
Xubili <guivpw68@gmail.com>
2020-10-20 04:52:21 -0300

    improved helpCommand code

--- bot/src/commands/help.ts
+++ bot/src/commands/help.ts
@@ -1,18 +1,19 @@
 import { Message } from 'discord.js'
 
 const helpCommand = async (message: Message) => {
-	await message.author.send(
-		`Oi **<@${message.author.id}>**, a ajuda está a caminho! :owl:`
-	)
-	await message.author.send(
-		'**Aqui está uma lista completinha com todos meus comandos** \nCaso não encontre o quê procura, entre no canal `#tech-suporte` no servidor e peça ajuda ao time de organizadores.'
-	)
-	await message.author.send(
-		'>>> **ajuda, help** ou **comandos** \nEnvia uma lista contendo todos os comandos do Discoder. \nExemplo: ``Você pode me dar uma ajuda?``\n'
-	)
-	await message.author.send(
-		'>>> **agendar mentoria** \nExecute esse comando para iniciar o guia de agendamento de mentoria. \nExemplo: ``Quero agendar uma mentoria``'
-	)
+	const messageSender = async (content: string) => {
+		return await message.author.send(content)
+	}
+	const messages = [
+		`Oi **<@${message.author.id}>**, a ajuda está a caminho! :owl:`,
+		'**Aqui está uma lista completinha com todos meus comandos** \nCaso não encontre o quê procura, entre no canal `#tech-suporte` no servidor e peça ajuda ao time de organizadores.',
+		'>>> **ajuda, help** ou **comandos** \nEnvia uma lista contendo todos os comandos do Discoder. \nExemplo: ``Você pode me dar uma ajuda?``\n',
+		'>>> **agendar mentoria** \nExecute esse comando para iniciar o guia de agendamento de mentoria. \nExemplo: ``Quero agendar uma mentoria``',
+		'>>> **escolher categoria** \nExecute esse comando para escolher a categoria da mentoria desejada. \nExemplo: ``Escolher categoria #UX``',
+		'>>> **escolher mentor** \nExecute esse comando para escolher o mentor disponível, a partir da categoria escolhida. \nExemplo: ``Escolher mentor Guilherme``',
+	]
+
+	return messages.forEach((eachMessage: string) => messageSender(eachMessage))
 }
 
 export default helpCommand
