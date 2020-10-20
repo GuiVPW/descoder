16ce7e47c717cc6755d292234ffda8626762940d
Xubili <guivpw68@gmail.com>
2020-10-20 04:52:04 -0300

    added deleteMessages function

--- bot/src/commands/delete.ts
+++ bot/src/commands/delete.ts
@@ -1,19 +1,26 @@
-import { Message, MessageEmbed } from 'discord.js'
+import { Message, MessageEmbed, MessageManager } from 'discord.js'
 
 const deleteAllCommand = async (
 	message: Message,
 	channel: 'dm' | 'text' | 'news'
 ) => {
-	console.log('guild', message.guild!.name)
-
-	// if (channel === 'text' && message.guild.roles.cache.find() {
-	const allMessages = await message.channel.messages.channel.bulkDelete(100)
-	if (allMessages)
-		await message.reply(
-			new MessageEmbed({ timestamp: new Date() })
-				.setColor('#fafafa')
-				.setTitle('Deletado 100 mensagens num piscar de olhos! :star_struck:')
+	const deleteMessages = async () => {
+		const allMessages = await message.channel.messages.channel.bulkDelete(100)
+		if (allMessages)
+			await message.reply(
+				new MessageEmbed({ timestamp: new Date() })
+					.setColor('#fafafa')
+					.setTitle('Deletado 100 mensagens num piscar de olhos! :star_struck:')
+			)
+	}
+	if (
+		channel === 'text' &&
+		message.member?.hasPermission(
+			'MANAGE_MESSAGES' || message.member.hasPermission('ADMINISTRATOR')
 		)
+	)
+		deleteMessages()
+	else if (channel === 'dm') deleteMessages()
 }
 
 export default deleteAllCommand
