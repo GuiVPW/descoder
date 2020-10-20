329fb27ca1c633070c76c7a09ccb16d518ad696e
Xubili <guivpw68@gmail.com>
2020-10-20 02:52:01 -0300

    added welcome message

--- api/src/constants/routes.ts
+++ api/src/constants/routes.ts
@@ -1,7 +1,11 @@
-import { Router } from 'express'
+import { Router, Request, Response } from 'express'
 import * as users from '../controllers/UserControllers'
 
 const routes = Router()
+
+routes.get('/', (req: Request, res: Response) => {
+	res.send('Bem-vindo')
+})
 
 routes.get('/users', users.indexAll)
 routes.get('/user/:id', users.index)
