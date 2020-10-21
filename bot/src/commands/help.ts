de1eb660e875652820ec7a63d86cd3fcc3bd2cc1
Xubili <guivpw68@gmail.com>
2020-10-20 23:48:32 -0300

    added more help commands

--- bot/src/commands/help.ts
+++ bot/src/commands/help.ts
@@ -1,19 +1,28 @@
 import { Message } from 'discord.js'
 
 const helpCommand = async (message: Message) => {
-	const messageSender = async (content: string) => {
-		return await message.author.send(content)
-	}
 	const messages = [
 		`Oi **<@${message.author.id}>**, a ajuda está a caminho! :owl:`,
 		'**Aqui está uma lista completinha com todos meus comandos** \nCaso não encontre o quê procura, entre no canal `#tech-suporte` no servidor e peça ajuda ao time de organizadores.',
 		'>>> **ajuda, help** ou **comandos** \nEnvia uma lista contendo todos os comandos do Discoder. \nExemplo: ``Você pode me dar uma ajuda?``\n',
-		'>>> **agendar mentoria** \nExecute esse comando para iniciar o guia de agendamento de mentoria. \nExemplo: ``Quero agendar uma mentoria``',
-		'>>> **escolher categoria** \nExecute esse comando para escolher a categoria da mentoria desejada. \nExemplo: ``Escolher categoria #UX``',
-		'>>> **escolher mentor** \nExecute esse comando para escolher o mentor disponível, a partir da categoria escolhida. \nExemplo: ``Escolher mentor Guilherme``',
+		'>>> **agendar mentoria** \nInicia o guia de agendamento de mentoria. \nExemplo: ``Quero agendar uma mentoria``',
+		'>>> **escolher categoria** \nEscolhe a categoria da mentoria desejada. \nExemplo: ``Escolher categoria #técnica``',
+		'>>> **escolher mentor** \nExibe os mentor disponíveis, a partir da categoria escolhida. \nExemplo: ``Escolher mentor Guilherme``',
+		'>>> **participando** \nInicia o guia de validação do time. \nExemplo: ``Estou participando?``',
+		'>>> **buscar time**\nBusca pelo repositório da equipe para validar se está participando ou não do evento. \nExemplo: ``Buscar time teste``',
+		'>>> **segredos** :shushing_face:\nComandos secretos que poucas pessoas irão descobrir...',
 	]
 
-	return messages.forEach((eachMessage: string) => messageSender(eachMessage))
+	if (message.channel.type === 'text')
+		messages.unshift(
+			`Para utilizar os comandos abaixo, me chame por aqui <@${process.env.BOT_ID}>.\n`
+		)
+
+	return messages.forEach(async (eachMessage: string) =>
+		message.channel.type === 'dm'
+			? await message.author.send(eachMessage)
+			: await message.channel.send(eachMessage)
+	)
 }
 
 export default helpCommand
