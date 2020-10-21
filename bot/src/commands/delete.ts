07f22d94e24ef1b18ba66a88566c906ff9baa56a
Xubili <guivpw68@gmail.com>
2020-10-20 23:47:07 -0300

    imprtoved delete all messages command

--- bot/src/commands/delete.ts
+++ bot/src/commands/delete.ts
@@ -1,26 +1,21 @@
-import { Message, MessageEmbed, MessageManager } from 'discord.js'
+import { Message, MessageEmbed } from 'discord.js'
 
-const deleteAllCommand = async (
-	message: Message,
-	channel: 'dm' | 'text' | 'news'
-) => {
-	const deleteMessages = async () => {
-		const allMessages = await message.channel.messages.channel.bulkDelete(100)
-		if (allMessages)
+const deleteAllCommand = async (message: Message) => {
+	if (
+		message.member?.hasPermission(
+			'MANAGE_MESSAGES' || message.member.hasPermission('ADMINISTRATOR')
+		)
+	) {
+		const deleteAllMessages = await message.channel.messages.channel.bulkDelete(
+			100
+		)
+		if (deleteAllMessages)
 			await message.reply(
 				new MessageEmbed({ timestamp: new Date() })
 					.setColor('#fafafa')
 					.setTitle('Deletado 100 mensagens num piscar de olhos! :star_struck:')
 			)
 	}
-	if (
-		channel === 'text' &&
-		message.member?.hasPermission(
-			'MANAGE_MESSAGES' || message.member.hasPermission('ADMINISTRATOR')
-		)
-	)
-		deleteMessages()
-	else if (channel === 'dm') deleteMessages()
 }
 
 export default deleteAllCommand
